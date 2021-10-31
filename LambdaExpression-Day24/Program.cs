3c2c51949f932d782cb9db59c7f09a8307518d37
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-31 09:57:23 +0530

    first commit

--- LambdaExpression-Day24/Program.cs
+++ LambdaExpression-Day24/Program.cs
@@ -6,7 +6,21 @@ namespace LambdaExpression_Day24
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Lambda Expression program");
+            static void Main(string[] args)
+            {
+                List<Person> listPersonInCity = new List<Person>();
+                AddRecords(listPersonInCity);
+                Retrieving_TopTwoRecord_ForAgels_LessThanSixty(listPersonInCity);
+                CheckingForTeenagerPerson(listPersonInCity);
+                AllPersonsAverageAge(listPersonInCity);
+                CheckNameExistOrNot(listPersonInCity);
+                SkipPersonsLessThan60(listPersonInCity);
+                RemoveSpecificPerson(listPersonInCity);
+                Console.ReadKey();
+            }
         }
+
+            
+           
     }
 }
