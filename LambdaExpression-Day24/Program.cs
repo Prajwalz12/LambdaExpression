83b7dcbf0488a5c42275ee34dc86c93bc3e2d612
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-31 09:17:50 +0530

    first commit

---
+++ LambdaExpression-Day24/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaExpression_Day24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda Expression program");
+        }
+    }
+}
